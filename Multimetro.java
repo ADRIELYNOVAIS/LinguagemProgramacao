4d93b6c4b9449ccc5b3980265b9f7d5168ff6892
Adriely Novais <adriely.novais@cepedi.org.br>
2023-03-21 23:58:47 -0300

    implementao de calculo da saude dod multimetro

--- Multimetro.java
+++ Multimetro.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 public class Multimetro {
     public static void main(String [] args) {
 
-        double measure = 0, result = 0, detour;
+        double measure = 0, result = 0, detour, healthMultimeter;
         double [] number = new double[10];
 
         Scanner scanner = new Scanner(System.in);
@@ -17,14 +17,20 @@ public class Multimetro {
 
         double media = measure / 10; //calcula a media dos elementos
 
+        healthMultimeter = media - (media - (media/10));
+
         for (int i = 0; i < 10; i++) {
             result += (number[i] - media) * (number[i] - media); //valor do elemento menos a media dos elementos
         }
 
         detour = Math.sqrt( result / 9); //calculo do desfio
 
-        System.out.printf("o valor do desvio padrao e: %.2f", detour);
+        if( detour > healthMultimeter){
+            System.out.printf("o multimetro esta com problemas e nao pode ser utilizado");
+        } else{
 
+        System.out.printf("a saude do multimetro esta ok. E o valor do desvio padrao e: %.2f", detour);
+        }
         scanner.close();
     }
 }
