6c714f406b536b32a4e0e10a348aace5420d1e5b
Adriely Novais <adriely.novais@cepedi.org.br>
2023-03-25 23:49:29 -0300

    versao alguma coisa para continuidade

--- batalhaNaval.java
+++ batalhaNaval.java
@@ -4,13 +4,14 @@ public class batalhaNaval {
     public static void main(String [] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int line, column, option = 0;
+        int line, column, option1 = 0,  option2 =0;
         //char [][] matrix1 =  {{'-','-','-','-','@'}, {'-','@','-','-','-'}, {'-','-','-','@','-'}, {'@','-','-','-','-'}, {'-','-','-','-','@'}}; //primeiro declara a matriz e depois inicializa (ver inicializacao)
-        char [][] matrix1 =  new char[5][5]; //matrix troca, ou seja, @ vira X
+        char [][] matrix1 =  new char[5][5];
+        char [][] matrix2 =  new char[5][5]; //matrix troca, ou seja, @ vira X
 
         System.out.println("Welcome to Battleship!!!");
 
-        while( option != 5 ) {
+        while( option1  != 5 ) {
 
         System.out.println("escolha uma linha e coluna: ");
         line = scanner.nextInt();
@@ -22,7 +23,7 @@ public class batalhaNaval {
         // preencher o tabuleiro de acordo com as coordenadas passadas
         if( line < 5 && column < 5 ) {
 
-            if( option == 0 ){
+            if( option1  == 0 ){
                 matrix1[line][column] = '@';
             } else{
 
@@ -40,15 +41,69 @@ public class batalhaNaval {
                     }}
 
             }
+            option1  +=1;
         } else{
             System.out.println("Invalid coordinates. Choose different coordinates.");
+            option1  -=1;
         }
               
-        option +=1;
+        
     }
 
+
+    while( option2  != 5 ) {
+
+        System.out.println("escolha uma linha2 e coluna2: ");
+        line = scanner.nextInt();
+        column = scanner.nextInt();
+
+        //System.out.println("escolha uma coluna: ");
+        
+        
+        // preencher o tabuleiro de acordo com as coordenadas passadas
+        if( line < 5 && column < 5 ) {
+
+            if( option2  == 0 ){
+                matrix2[line][column] = 'X';
+            } else{
+
+                for( int i = 0; i <5; i++){
+                    for( int j = 0; j<5; j++){
+
+                        if( matrix2[i][j] != 'X' ){
+                           // System.out.println("You already have a ship there. Choose different coordinates"); // como imprimir apenas uma vez
+                          // como tratar a questao de nao pedir de novo coordenadas diferentes
+                          matrix2[i][j] = '-';
+                        
+                        }else{
+                            matrix2[line][column] = 'X';
+                        }
+                    }}
+
+            }
+            option2  +=1;
+        } else{
+            System.out.println("Invalid coordinates. Choose different coordinates.");
+            option2  -=1;
+        }   
+    }
+
+        for( int i =0; i <5; i++){
+            for(int j=0; j <5; j++){
+                if( matrix2[i][j] == matrix1[i][j] ){
+                    matrix2[i][j] ='X';
+                }else{
+                    matrix2[i][j] ='-'; // e o erro no alvo??
+                }
+            }
+        }
+
     System.out.println("Matriz Inicial:");
-    prnMatrix(matrix1); 
+    prnMatrix(matrix1);
+
+    System.out.println("Matriz Dois:");
+    prnMatrix(matrix2);
+
 
     scanner.close();
 }
