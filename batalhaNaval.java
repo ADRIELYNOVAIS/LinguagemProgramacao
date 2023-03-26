71fb60d972a185ccc2bcde4f054adb3c1b8097ce
Adriely Novais <adriely.novais@cepedi.org.br>
2023-03-26 19:04:53 -0300

    falta tratamentos

--- batalhaNaval.java
+++ batalhaNaval.java
@@ -4,10 +4,10 @@ public class batalhaNaval {
     public static void main(String [] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int line, column, option1 = 0,  option2 =0;
-        //char [][] matrix1 =  {{'-','-','-','-','@'}, {'-','@','-','-','-'}, {'-','-','-','@','-'}, {'@','-','-','-','-'}, {'-','-','-','-','@'}}; //primeiro declara a matriz e depois inicializa (ver inicializacao)
+        int line, column, option1 = 0;
+       
         char [][] matrix1 =  new char[5][5];
-        char [][] matrix2 =  new char[5][5]; //matrix troca, ou seja, @ vira X
+        char [][] matrix2 =  new char[5][5];
 
         System.out.println("Welcome to Battleship!!!");
 
@@ -17,10 +17,6 @@ public class batalhaNaval {
         line = scanner.nextInt();
         column = scanner.nextInt();
 
-        //System.out.println("escolha uma coluna: ");
-        
-        
-        // preencher o tabuleiro de acordo com as coordenadas passadas
         if( line < 5 && column < 5 ) {
 
             if( option1  == 0 ){
@@ -50,43 +46,6 @@ public class batalhaNaval {
         
     }
 
-
-    while( option2  != 5 ) {
-
-        System.out.println("escolha uma linha2 e coluna2: ");
-        line = scanner.nextInt();
-        column = scanner.nextInt();
-
-        //System.out.println("escolha uma coluna: ");
-        
-        
-        // preencher o tabuleiro de acordo com as coordenadas passadas
-        if( line < 5 && column < 5 ) {
-
-            if( option2  == 0 ){
-                matrix2[line][column] = 'X';
-            } else{
-
-                for( int i = 0; i <5; i++){
-                    for( int j = 0; j<5; j++){
-
-                        if( matrix2[i][j] != 'X' ){
-                           // System.out.println("You already have a ship there. Choose different coordinates"); // como imprimir apenas uma vez
-                          // como tratar a questao de nao pedir de novo coordenadas diferentes
-                          matrix2[i][j] = '-';
-                        
-                        }else{
-                            matrix2[line][column] = 'X';
-                        }
-                    }}
-
-            }
-            option2  +=1;
-        } else{
-            System.out.println("Invalid coordinates. Choose different coordinates.");
-            option2  -=1;
-        }   
-    }
 
         for( int i =0; i <5; i++){
             for(int j=0; j <5; j++){
