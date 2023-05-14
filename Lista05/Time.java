38a508244a5efba3909d6aaa5458311e1db64ec2
Adriely Novais <adriely.novais@cepedi.org.br>
2023-05-13 22:16:07 -0300

    classe time

---
+++ Lista05/Time.java
@@ -0,0 +1,71 @@
+package Lista05;
+
+public class Time {
+    private int valor;
+    
+    public Time( int valor ) {
+
+        if(validateTime(valor)){
+            this.valor = valor;
+        }else{
+            this.valor = 0;
+        }
+        
+    }
+    
+    private boolean validateTime(int timevalor){
+        return ((timevalor>=0) && (timevalor<86401));
+        //SegundosEmUmDia = 24*60*60 = 86400 
+    }
+
+    private int TimeTosegonds(int hora, int min, int seg){
+        //converte tudo em segundos
+        return hora*3600 + min*60 + seg;
+    }
+
+    public boolean isAm(int hora, int min, int seg){
+        int valor = this.TimeTosegonds(hora, min, seg);
+
+        if( validateTime(valor)){
+            if(valor < (86400/2)){
+                //verifica se a hora é anterior a 12horas
+                return true;
+            }else{
+                return false;
+            }
+        } else{
+            this.valor = 0;
+            return false;
+        }
+        
+    }
+
+
+    public int cron(Time outraHora){
+       /* retorna a quantidade de segundos transcorridos desde a hora
+        enviada como parâmetro, se o parâmetros outraHora for anterior ao tempo do objeto
+        considerar que o parâmetro se refere ao dia seguinte */
+        if( outraHora.valor < this.valor){
+           this.valor += 86400;
+        } 
+            return outraHora.valor - this.valor;
+            
+    }
+
+    public void addSegonds(int segs){
+        if(segs > 0)    {
+            if( valor > 86401){
+                // se for um novo dia, o valor que representa um dia é reiniciado
+                valor = 0;
+                valor +=segs;
+            }else{
+                valor +=segs;
+            }
+        }else{
+            segs = 0;
+            valor +=segs;
+        }
+    }
+
+
+}
