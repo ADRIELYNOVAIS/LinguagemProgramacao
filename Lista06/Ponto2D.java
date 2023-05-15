a7bef14d6d96771e994ba730534e10cb2929478d
Adriely Novais <adriely.novais@cepedi.org.br>
2023-05-15 00:14:00 -0300

    lista 6

---
+++ Lista06/Ponto2D.java
@@ -0,0 +1,41 @@
+package Lista06;
+
+public class Ponto2D {
+    private float x, y;
+
+    public Ponto2D( float x, float y ) {
+        this.x = x;
+        this.y = y;
+    }
+    public Ponto2D(){
+        x =0;
+        y =0;
+    }
+
+    public double distance( Ponto2D ponto ){
+        
+        double dist = 0;
+        float x = ponto.x - this.x;
+        float y = ponto.y - this.y;
+        dist = Math.sqrt((x * x) + (y * y));
+        return dist;
+    }
+
+    public double distance(){
+        double dist = 0;
+        float x = this.x - 0;
+        float y = this.y - 0;
+        dist = Math.sqrt((x * x) + (y * y));
+        return dist;
+    }
+
+    public double distance( float x, float y ) {
+        
+        double dist = 0;
+        float dx = x - this.x;
+        float dy = y - this.y;
+        dist = Math.sqrt(dx * dx + dy * dy);
+        return dist;
+    }
+    
+}
