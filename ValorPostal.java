741d85dcad0dce6bef76015d3ae4cda12ae538d2
Adriely Novais <adriely.novais@cepedi.org.br>
2023-11-03 11:57:40 -0300

    solução do desafio do valor postal

--- ValorPostal.java
+++ ValorPostal.java
@@ -1,23 +1,36 @@
 import java.util.Scanner;
 
-public class ValorPostal {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Informe o valor de n (maior ou igual a 12): ");
         int n = scanner.nextInt();
+        int a, b;
 
-        if (n < 12) {
-            System.out.println("n deve ser maior ou igual a 12.");
-        } else {
-            int a = 0;
-            while ((n - 4 * a) % 5 != 0) {
-                a++;
+        if (n >= 12) {
+            if (n % 4 == 0) {
+                a = n / 4;
+                b = 0;
+                System.out.printf("n = 4 * %d e 5 * %d%n", a, b);
+            } else if (n % 5 == 0) {
+                a = 0;
+                b = n / 5;
+                System.out.printf("n = 4 * %d e 5 * %d%n", a, b);
+            } else {
+                int i = 1;
+                while ((n / 4) - i >= 0) {
+                    a = (n / 4) - i;
+                    b = i;
+                    int j = 4 * a + 5 * b;
+                    if (n == j) {
+                        System.out.printf("n = 4 * %d e 5 * %d%n", a, b);
+                    }
+                    i++;
+                }
             }
-
-            int b = (n - 4 * a) / 5;
-
-            System.out.println("Para n = " + n + ", 4 * a = " + (4 * a) + " e 5 * b = " + (5 * b));
+        } else {
+            System.out.println("n deve ser maior ou igual a 12.");
         }
     }
 }
